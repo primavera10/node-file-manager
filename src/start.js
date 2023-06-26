afc1b3ef592d34e1d0320bd3f72f11beec62049a
Liza <lizavetayalak2000@gmail.com>
2023-06-27 01:13:38 +0200

    done file system operations

--- src/start.js
+++ src/start.js
@@ -1,37 +1,42 @@
 import path from 'node:path';
 import { fileURLToPath } from 'url';
 import fs from 'node:fs'
+import { createReadStream, createWriteStream } from 'fs'
 
 
 let directory = path.dirname(fileURLToPath(import.meta.url))
 const args = process.argv.splice(2)
 const user = args[0].match(/=(.+)/)[1]
-process.stdout.write(`Welcome to the File Manager, ${user}!\n`)
-process.stdout.write(directory + "\n")
 
+function writeToConsole() {
+    process.stdout.write(directory)
+    process.stdout.write('\n')
+}
+
+process.stdout.write(`Welcome to the File Manager, ${user}!\n`)
+writeToConsole()
 process.stdin.on('data', (chunk) => {
     const [_m, operation, arg] = chunk.toString().match(/([a-z]+) ?([^\r\n]+)?/);
     if (operation.startsWith('up')) {
-        directory =  path.resolve(directory, '..')
-        process.stdout.write(directory +'\n')
+        directory = path.resolve(directory, '..')
+        writeToConsole()
     }
-    if (operation.startsWith('cd')){
-       directory = path.resolve(directory, arg)
-        process.stdout.write(directory +'\n')
+    if (operation.startsWith('cd')) {
+        directory = path.resolve(directory, arg)
+        writeToConsole()
     }
-    if (operation === 'ls'){
-        let list = fs.readdirSync(directory, {withFileTypes: true})
-        console.log(list)
-         let dirs = []
-         let files = []
-         list.forEach(elem => {
-             if (elem.isFile()){
-                 files.push({ Name: elem.name, Type: 'file' })
-             } else {
-                 dirs.push({ Name: elem.name, Type: 'directory' })
-             }
-         })
-        dirs.sort((a,b)=>{
+    if (operation === 'ls') {
+        let list = fs.readdirSync(directory, { withFileTypes: true })
+        let dirs = []
+        let files = []
+        list.forEach(elem => {
+            if (elem.isFile()) {
+                files.push({ Name: elem.name, Type: 'file' })
+            } else {
+                dirs.push({ Name: elem.name, Type: 'directory' })
+            }
+        })
+        dirs.sort((a, b) => {
             const nameA = a.Name.toUpperCase();
             const nameB = b.Name.toUpperCase();
             if (nameA < nameB) {
@@ -42,8 +47,7 @@ process.stdin.on('data', (chunk) => {
             }
             return 0;
         })
-        console.log(dirs)
-        files.sort((a,b)=>{
+        files.sort((a, b) => {
             const nameA = a.Name.toUpperCase();
             const nameB = b.Name.toUpperCase();
             if (nameA < nameB) {
@@ -54,11 +58,59 @@ process.stdin.on('data', (chunk) => {
             }
             return 0;
         })
-        console.table([...dirs, ...files],['Name','Type'])
+        console.table([...dirs, ...files], ['Name', 'Type'])
+        writeToConsole()
     }
+    if (operation === 'cat') {
+        const file = path.resolve(directory, arg);
+        console.log(file)
+        const readable = createReadStream(file)
+        readable.pipe(process.stdout)
+        writeToConsole()
+    }
+    if (operation === 'add'){
+        const newPath = path.resolve(directory, arg)
+        fs.writeFileSync(newPath, '')
+        writeToConsole()
+    }
+    if (operation === 'rn'){
+       const [arg1, arg2] = arg.split(' ')
+        const oldPath = path.resolve(directory, arg1)
+        const newPath = path.resolve(path.dirname(oldPath) ,arg2)
+        console.log(newPath)
+        fs.renameSync(oldPath, newPath)
+        writeToConsole()
+    }
+    if (operation === 'cp'){
+        const [arg1, arg2] = arg.split(' ')
+        const oldPath = path.resolve(directory, arg1)
+        const newPath = path.resolve(directory, arg2, path.basename(oldPath));
+        fs.writeFileSync(newPath, '')
+        const readable = createReadStream(oldPath)
+        const writable = createWriteStream(newPath)
+        readable.pipe(writable)
+        writeToConsole()
+    }
+    if (operation === 'mv'){
+        const [arg1, arg2] = arg.split(' ')
+        const oldPath = path.resolve(directory, arg1)
+        const newPath = path.resolve(directory, arg2, path.basename(oldPath));
+        fs.writeFileSync(newPath, '')
+        const readable = createReadStream(oldPath)
+        const writable = createWriteStream(newPath)
+        readable.pipe(writable)
+        readable.on('end', () => fs.unlinkSync(oldPath))
+        writeToConsole()
+    }
+    if (operation === 'rm'){
+        const pathToDelete = path.resolve(directory, arg)
+        fs.unlinkSync(pathToDelete)
+        writeToConsole()
+    }
+
 })
 
 // process.stdout.end()
-process.stdin.on('end', () => {
-    process.stdout.write(`Thank you for using File Manager, ${user}, goodbye!`)
+process.on('SIGINT', () => {
+    console.log(`Thank you for using File Manager, ${user}, goodbye!`)
 })
